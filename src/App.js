4d07f07f1954f28a0aecda494642080d3e73cbb8
MyNameMatters <ellistanner.3@gmail.com>
2022-04-06 23:41:28 -0600

    testing out useState hook

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     {id:1, title: 'Groceries', amount: 67.32, date: new Date(2022, 3, 7)},
     {id:2, title: 'Car Insurance', amount: 111.43, date: new Date(2022, 3, 14)},
     {id:3, title: 'Credit Card', amount: 41.67, date: new Date(2022, 3, 17)},
-    {id:4, title: 'Dinner', amount: 18.29, date: new Date(2022, 3, 23)},
+    {id:4, title: 'T-Shirt', amount: 18.29, date: new Date(2022, 3, 23)},
   ]
   return (
     <div>
