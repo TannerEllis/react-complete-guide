f54c32b740b40c59dcf49ff7ca65c55023d65445
MyNameMatters <ellistanner.3@gmail.com>
2022-04-08 09:46:42 -0600

    added comment

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
+  //this will be dynameic data with state
   const expenses = [
     {id:1, title: 'Groceries', amount: 67.32, date: new Date(2022, 3, 7)},
     {id:2, title: 'Car Insurance', amount: 111.43, date: new Date(2022, 3, 14)},
     {id:3, title: 'Credit Card', amount: 41.67, date: new Date(2022, 3, 17)},
     {id:4, title: 'T-Shirt', amount: 18.29, date: new Date(2022, 3, 23)},
-  ]
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
