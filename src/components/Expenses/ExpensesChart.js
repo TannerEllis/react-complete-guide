2f9a76dcd10030d2c98708916a065074d9335b97
MyNameMatters <ellistanner.3@gmail.com>
2022-04-16 18:03:02 -0600

    added Chart components to display the chart in the expenses

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,31 @@
+import { prependOnceListener } from "process";
+import React from "react";
+import Chart from '../Chart/Chart'
+
+const ExpenseChart = (props) => {
+    const chartDataPoints = [
+        {label: 'Jan', value: 0},
+        {label: 'Feb', value: 1},
+        {label: 'Mar', value: 2},
+        {label: 'Apr', value: 3},
+        {label: 'May', value: 4},
+        {label: 'Jun', value: 5},
+        {label: 'Jul', value: 6},
+        {label: 'Aug', value: 7},
+        {label: 'Sep', value: 8},
+        {label: 'Oct', value: 9},
+        {label: 'Nov', value: 10},
+        {label: 'Dev', value: 11}
+    ];
+
+    for (const expense of props.expenses) {
+        const expenseMonth = expense.date.getMonth();
+        chartDataPoints[expenseMonth].value += expense.amount;
+    };
+
+    return (
+        <Chart dataPoints={chartDataPoints}/>
+    )
+};
+
+export default ExpenseChart;
